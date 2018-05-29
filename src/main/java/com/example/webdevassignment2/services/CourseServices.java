fb0ec76b55e110d8d022251a51c6761e475043be
Priyaranjan1993 <priyaranjan9090@gmail.com>
2018-05-29 10:48:17 -0400

    create course and delete course service committed

--- src/main/java/com/example/webdevassignment2/services/CourseServices.java
+++ src/main/java/com/example/webdevassignment2/services/CourseServices.java
@@ -1,13 +1,19 @@
 package com.example.webdevassignment2.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.webdevassignment2.models.Course;
 import com.example.webdevassignment2.repositories.CourseRepository;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class CourseServices {
 	@Autowired
 	CourseRepository courseRepository;
@@ -16,5 +22,17 @@ public class CourseServices {
 	@GetMapping("/api/course")
 	public Iterable<Course> findAllCourses() {
 		return courseRepository.findAll();
+	}
+
+	@PostMapping("/api/course")
+	public Course createCourse
+	(@RequestBody Course course) {
+		return courseRepository.save(course);
+	}
+	
+	@DeleteMapping("/api/course/{courseId}")
+	public void deleteCourse(
+	@PathVariable("courseId") int id) {
+	courseRepository.deleteById(id);
 	}
 }
