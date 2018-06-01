81f92dbae78c4ab5d548c32b5246fc4de382243c
Priyaranjan1993 <priyaranjan9090@gmail.com>
2018-06-01 08:55:34 -0400

    lesson repository committed

---
+++ src/main/java/com/example/webdevassignment2/repositories/LessonRepository.java
@@ -0,0 +1,8 @@
+package com.example.webdevassignment2.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import com.example.webdevassignment2.models.*;
+
+public interface LessonRepository extends CrudRepository<Lesson, Integer> {
+
+}
