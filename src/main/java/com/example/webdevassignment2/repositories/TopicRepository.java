386dca4c44cf5fcbc80b421869a2eeac3780f775
Priyaranjan1993 <priyaranjan9090@gmail.com>
2018-06-03 12:02:32 -0400

    commit of topic model,service and repository and minor bug fixing

---
+++ src/main/java/com/example/webdevassignment2/repositories/TopicRepository.java
@@ -0,0 +1,9 @@
+package com.example.webdevassignment2.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.webdevassignment2.models.Topic;
+
+public interface TopicRepository extends CrudRepository<Topic, Integer> {
+
+}
